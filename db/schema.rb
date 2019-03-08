7b5af314c5569c59f0457d001e9c015a63c44c33
stubrew24 <sewelly@gmail.com>
2019-03-08 10:10:57 +0000

    Done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_215941) do
+ActiveRecord::Schema.define(version: 2019_03_08_093406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,13 +29,13 @@ ActiveRecord::Schema.define(version: 2019_03_07_215941) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
     t.text "content"
     t.integer "taproom_id"
     t.string "img_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "published"
+    t.string "title"
   end
 
   create_table "reviews", force: :cascade do |t|
